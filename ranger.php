89ee192562ba3721f3b77805e7df95f309e7c1ee
laguel <kevin.laguel@digiwin.fr>
2021-10-05 14:26:51 +0200

    enregistre en json les paramètres

---
+++ ranger.php
@@ -0,0 +1,14 @@
+<?php
+function ranger($words){
+    $file = "contenu.json";
+    asort($words);
+    $finalArray = [];
+
+    foreach($words as $word) {
+        if($word != "ranger.php")
+        array_push($finalArray, $word);
+    }
+    file_put_contents($file,json_encode($finalArray));
+
+}
+ranger($argv);
